a0cc24779ae9f382e7eedf4a2815a793bc900af9
Kaspar Rosin <kaspar@outl1ne.com>
2024-01-09 17:05:18 +0200

    feat: work on nova_sync logic

---
+++ README.md
@@ -0,0 +1,1 @@
+# nova-release-tracking
---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const semver = require("semver");
+
+const nova_url = "https://nova.laravel.com";
+const repo = "outl1ne/nova-multiselect-field";
+const github_api_url = "https://api.github.com";
+const latest_release_url = `${github_api_url}/repos/${repo}/releases/latest`;
+
+async function main() {
+  const releases_url = await fetch(`${nova_url}/packages.json`)
+    .then((data) => data.json())
+    .then((res) => Object.keys(res["includes"])[0]);
+
+  const nova_releases = await fetch(`${nova_url}/${releases_url}`)
+    .then((data) => data.json())
+    .then((res) => res["packages"]["laravel/nova"]);
+
+  const nova_prod_releases = Object.keys(nova_releases)
+    .filter((release) => !release.includes("dev"))
+    .sort(semver.compare)
+    .map(semver.clean);
+
+  const current_release = await fetch(latest_release_url)
+    .then((data) => data.json())
+    .then((res) => res["tag_name"]);
+
+  let next_prod_release;
+  let has_more_releases = false;
+  const last_nova_release = nova_prod_releases[nova_prod_releases.length - 1];
+
+  if (current_release) {
+    const current_release_idx = releases_sorted.indexOf(current_release);
+    next_prod_release = releases_sorted[current_release_idx + 1];
+    has_more_releases = next_prod_release !== last_nova_release;
+  } else {
+    // If we don't have existing releases, let's download the two latest
+    next_prod_release = releases_sorted.slice(-2)[0];
+    has_more_releases = true;
+  }
+
+  console.log("current release:", current_release);
+  console.log("next_prod_release:", next_prod_release);
+  console.log("has_more_releases:", has_more_releases);
+}
+
+try {
+  main();
+} catch (error) {
+  core.setFailed(error.message);
+}
